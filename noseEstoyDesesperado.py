d59b4c38862271556c467c1fea03d3f3ea905109
Franisis <f.guzmana@uniandes.edu.co>
2022-08-23 10:17:29 -0500

    It Works!!!

--- noseEstoyDesesperado.py
+++ noseEstoyDesesperado.py
@@ -1,14 +1,19 @@
-isGood = False
+
+
+
+
 uni= []
 parenCount=0
 keyCount=0
 def read_AF(t): 
-
+    global parenCount, keyCount
+    isGood = True
     word = ''
     for i in range(len(t)):
         
         word += t[i]
-        print(word)
+        print(word, isGood)
+        
         if " " in word:
             word = ''
         if word == "vars" and ";" in t:
@@ -32,6 +37,28 @@ def read_AF(t):
             parenCount+=1
         if t[i] == '{' or t[i] == '}':
             keyCount+=1     
+        if word == 'while' and parenCount%2==0 and keyCount%2==0 : 
+            word = ''
+            print('understood')
+        if word == 'do' and parenCount%2==0 and keyCount%2==0 : 
+            word = ''
+            print('understood')
+        if t == '':
+            print('understood')
+            return True 
+        if word == 'walk' or 'free' or 'drop':
+            word == ''
+            print('understood')
+    print("wtf")
+    if parenCount%2>0 or keyCount%2>0 : 
+        print('got it')
+        isGood=False
+    if word != '':
+        print("I don't get it")
+        isGood =False
+    return isGood, keyCount,parenCount
+            
+
         
 def readInitEnd(t):
     global isGood
@@ -43,7 +70,9 @@ def readInitEnd(t):
 def main():
     global isGood
     t = input("Ingrese el programa: ")
-    readInitEnd(t)
-    read_AF(t)
+    isOk = read_AF(t)
+    print(isOk)
+    
+
 main()
     
